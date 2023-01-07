758e1d8204e45bd7cc00ab43c3479048dc7a6ea9
Sangjin-Lee <sjlee@enc.hanyang.ac.kr>
2023-01-08 02:48:40 +0900

    Removed PL-DIMM-0

--- ps.srcs/sources_1/bd/design_1/hdl/design_1_wrapper.v
+++ ps.srcs/sources_1/bd/design_1/hdl/design_1_wrapper.v
@@ -1,7 +1,7 @@
 //Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
 //--------------------------------------------------------------------------------
 //Tool Version: Vivado v.2019.1 (win64) Build 2552052 Fri May 24 14:49:42 MDT 2019
-//Date        : Sat Jan  7 22:43:06 2023
+//Date        : Sun Jan  8 00:08:34 2023
 //Host        : LAPTOP-HS-5597 running 64-bit major release  (build 9200)
 //Command     : generate_target design_1_wrapper.bd
 //Design      : design_1_wrapper
@@ -14,22 +14,7 @@ module design_1_wrapper
     M2_1_V33_EN,
     M2_2_PERSTn,
     M2_2_V33_EN,
-    c0_init_calib_complete_0,
     c0_init_calib_complete_1,
-    ddr4_rtl_0_act_n,
-    ddr4_rtl_0_adr,
-    ddr4_rtl_0_ba,
-    ddr4_rtl_0_bg,
-    ddr4_rtl_0_ck_c,
-    ddr4_rtl_0_ck_t,
-    ddr4_rtl_0_cke,
-    ddr4_rtl_0_cs_n,
-    ddr4_rtl_0_dq,
-    ddr4_rtl_0_dqs_c,
-    ddr4_rtl_0_dqs_t,
-    ddr4_rtl_0_odt,
-    ddr4_rtl_0_par,
-    ddr4_rtl_0_reset_n,
     ddr4_rtl_1_act_n,
     ddr4_rtl_1_adr,
     ddr4_rtl_1_ba,
@@ -48,8 +33,6 @@ module design_1_wrapper
     diff_clock_rtl_0_clk_p,
     diff_clock_rtl_1_clk_n,
     diff_clock_rtl_1_clk_p,
-    diff_clock_rtl_2_clk_n,
-    diff_clock_rtl_2_clk_p,
     diff_clock_rtl_3_clk_n,
     diff_clock_rtl_3_clk_p,
     diff_clock_rtl_4_clk_n,
@@ -75,22 +58,7 @@ module design_1_wrapper
   output [0:0]M2_1_V33_EN;
   output [0:0]M2_2_PERSTn;
   output [0:0]M2_2_V33_EN;
-  output c0_init_calib_complete_0;
   output c0_init_calib_complete_1;
-  output ddr4_rtl_0_act_n;
-  output [16:0]ddr4_rtl_0_adr;
-  output [1:0]ddr4_rtl_0_ba;
-  output [1:0]ddr4_rtl_0_bg;
-  output [0:0]ddr4_rtl_0_ck_c;
-  output [0:0]ddr4_rtl_0_ck_t;
-  output [1:0]ddr4_rtl_0_cke;
-  output [1:0]ddr4_rtl_0_cs_n;
-  inout [71:0]ddr4_rtl_0_dq;
-  inout [17:0]ddr4_rtl_0_dqs_c;
-  inout [17:0]ddr4_rtl_0_dqs_t;
-  output [1:0]ddr4_rtl_0_odt;
-  output ddr4_rtl_0_par;
-  output ddr4_rtl_0_reset_n;
   output ddr4_rtl_1_act_n;
   output [16:0]ddr4_rtl_1_adr;
   output [1:0]ddr4_rtl_1_ba;
@@ -109,8 +77,6 @@ module design_1_wrapper
   input [0:0]diff_clock_rtl_0_clk_p;
   input [0:0]diff_clock_rtl_1_clk_n;
   input [0:0]diff_clock_rtl_1_clk_p;
-  input diff_clock_rtl_2_clk_n;
-  input diff_clock_rtl_2_clk_p;
   input diff_clock_rtl_3_clk_n;
   input diff_clock_rtl_3_clk_p;
   input [0:0]diff_clock_rtl_4_clk_n;
@@ -137,22 +103,7 @@ module design_1_wrapper
   wire [0:0]M2_1_V33_EN;
   wire [0:0]M2_2_PERSTn;
   wire [0:0]M2_2_V33_EN;
-  wire c0_init_calib_complete_0;
   wire c0_init_calib_complete_1;
-  wire ddr4_rtl_0_act_n;
-  wire [16:0]ddr4_rtl_0_adr;
-  wire [1:0]ddr4_rtl_0_ba;
-  wire [1:0]ddr4_rtl_0_bg;
-  wire [0:0]ddr4_rtl_0_ck_c;
-  wire [0:0]ddr4_rtl_0_ck_t;
-  wire [1:0]ddr4_rtl_0_cke;
-  wire [1:0]ddr4_rtl_0_cs_n;
-  wire [71:0]ddr4_rtl_0_dq;
-  wire [17:0]ddr4_rtl_0_dqs_c;
-  wire [17:0]ddr4_rtl_0_dqs_t;
-  wire [1:0]ddr4_rtl_0_odt;
-  wire ddr4_rtl_0_par;
-  wire ddr4_rtl_0_reset_n;
   wire ddr4_rtl_1_act_n;
   wire [16:0]ddr4_rtl_1_adr;
   wire [1:0]ddr4_rtl_1_ba;
@@ -171,8 +122,6 @@ module design_1_wrapper
   wire [0:0]diff_clock_rtl_0_clk_p;
   wire [0:0]diff_clock_rtl_1_clk_n;
   wire [0:0]diff_clock_rtl_1_clk_p;
-  wire diff_clock_rtl_2_clk_n;
-  wire diff_clock_rtl_2_clk_p;
   wire diff_clock_rtl_3_clk_n;
   wire diff_clock_rtl_3_clk_p;
   wire [0:0]diff_clock_rtl_4_clk_n;
@@ -200,22 +149,7 @@ module design_1_wrapper
         .M2_1_V33_EN(M2_1_V33_EN),
         .M2_2_PERSTn(M2_2_PERSTn),
         .M2_2_V33_EN(M2_2_V33_EN),
-        .c0_init_calib_complete_0(c0_init_calib_complete_0),
         .c0_init_calib_complete_1(c0_init_calib_complete_1),
-        .ddr4_rtl_0_act_n(ddr4_rtl_0_act_n),
-        .ddr4_rtl_0_adr(ddr4_rtl_0_adr),
-        .ddr4_rtl_0_ba(ddr4_rtl_0_ba),
-        .ddr4_rtl_0_bg(ddr4_rtl_0_bg),
-        .ddr4_rtl_0_ck_c(ddr4_rtl_0_ck_c),
-        .ddr4_rtl_0_ck_t(ddr4_rtl_0_ck_t),
-        .ddr4_rtl_0_cke(ddr4_rtl_0_cke),
-        .ddr4_rtl_0_cs_n(ddr4_rtl_0_cs_n),
-        .ddr4_rtl_0_dq(ddr4_rtl_0_dq),
-        .ddr4_rtl_0_dqs_c(ddr4_rtl_0_dqs_c),
-        .ddr4_rtl_0_dqs_t(ddr4_rtl_0_dqs_t),
-        .ddr4_rtl_0_odt(ddr4_rtl_0_odt),
-        .ddr4_rtl_0_par(ddr4_rtl_0_par),
-        .ddr4_rtl_0_reset_n(ddr4_rtl_0_reset_n),
         .ddr4_rtl_1_act_n(ddr4_rtl_1_act_n),
         .ddr4_rtl_1_adr(ddr4_rtl_1_adr),
         .ddr4_rtl_1_ba(ddr4_rtl_1_ba),
@@ -234,8 +168,6 @@ module design_1_wrapper
         .diff_clock_rtl_0_clk_p(diff_clock_rtl_0_clk_p),
         .diff_clock_rtl_1_clk_n(diff_clock_rtl_1_clk_n),
         .diff_clock_rtl_1_clk_p(diff_clock_rtl_1_clk_p),
-        .diff_clock_rtl_2_clk_n(diff_clock_rtl_2_clk_n),
-        .diff_clock_rtl_2_clk_p(diff_clock_rtl_2_clk_p),
         .diff_clock_rtl_3_clk_n(diff_clock_rtl_3_clk_n),
         .diff_clock_rtl_3_clk_p(diff_clock_rtl_3_clk_p),
         .diff_clock_rtl_4_clk_n(diff_clock_rtl_4_clk_n),
